82a2b5aaec614e7a5ac7702473ce35c548cab698
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-12-13 14:35:39 +0900

    SierpinskiGasket

---
+++ src/SierpinskiGasket.cpp
@@ -0,0 +1,9 @@
+//
+//  SierpinskiGasket.cpp
+//  FractalTest
+//
+//  Created by SakuragiYoshimasa on 2015/12/13.
+//
+//
+
+#include "SierpinskiGasket.h"
---
+++ src/SierpinskiGasket.h
@@ -0,0 +1,17 @@
+//
+//  SierpinskiGasket.hpp
+//  FractalTest
+//
+//  Created by SakuragiYoshimasa on 2015/12/13.
+//
+//
+
+#ifndef SierpinskiGasket_hpp
+#define SierpinskiGasket_hpp
+
+#include "ofMain.h"
+
+class SierpinskiGasket {
+
+};
+#endif /* SierpinskiGasket_hpp */
